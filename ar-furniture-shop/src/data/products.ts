cb289320611c675e2fc75c55460d46bec7d22d44
Ariel <ariel.chambaz@epitech.eu>
2024-11-30 11:56:27 +0700

    (+) Pictures

--- ar-furniture-shop/src/data/products.ts
+++ ar-furniture-shop/src/data/products.ts
@@ -3,34 +3,130 @@ import { Product } from '../types/Product';
 export const products: Product[] = [
     {
         id: 1,
-        name: 'Maneki',
-        price: 999.99,
+        name: 'Maneki Cat',
+        price: 19.99,
         modelUrl: '/product/1',
-        image: '/images/sofa-thumbnail.jpg',
+        image: '/images/IMG_3657.jpg',
         iosSrc: '/assets/maneki.usdz'
     },
     {
         id: 2,
-        name: 'Table de jardin',
-        price: 999.99,
+        name: 'Garden Table',
+        price: 199.99,
         modelUrl: '/product/2',
-        image: '/images/sofa-thumbnail.jpg',
+        image: '/images/IMG_3658.jpg',
         iosSrc: '/assets/AR-Code-1677660985492.usdz'
     },
     {
         id: 3,
-        name: 'Fauteil en cuir',
-        price: 999.99,
+        name: 'Leather Armchair',
+        price: 499.99,
         modelUrl: '/product/3',
-        image: '/images/sofa-thumbnail.jpg',
+        image: '/images/IMG_3659.jpg',
         iosSrc: '/assets/AR-Code-1683007596576.usdz'
     },
     {
         id: 4,
-        name: 'Table basse',
-        price: 999.99,
+        name: 'Coffee Table',
+        price: 149.99,
         modelUrl: '/product/4',
-        image: '/images/sofa-thumbnail.jpg',
+        image: '/images/IMG_3660.jpg',
         iosSrc: '/assets/AR-Code-1683007809279.usdz'
     },
+    {
+        id: 5,
+        name: 'Wooden Coffee Table',
+        price: 189.99,
+        modelUrl: '/product/5',
+        image: '/images/IMG_3661.jpg',
+        iosSrc: '/assets/AR-Code-1683008480369.usdz'
+    },
+    {
+        id: 6,
+        name: 'Adriano Table',
+        price: 249.99,
+        modelUrl: '/product/6',
+        image: '/images/IMG_3662.jpg',
+        iosSrc: '/assets/Adriano_Table.usdz'
+    },
+    {
+        id: 7,
+        name: 'Blaze Chair',
+        price: 149.99,
+        modelUrl: '/product/7',
+        image: '/images/IMG_3663.jpg',
+        iosSrc: '/assets/Blaze_Chair.usdz'
+    },
+    {
+        id: 8,
+        name: 'Swan Chair',
+        price: 229.99,
+        modelUrl: '/product/8',
+        image: '/images/IMG_3664.jpg',
+        iosSrc: '/assets/chair_swan.usdz'
+    },
+    {
+        id: 9,
+        name: 'Fender Guitar',
+        price: 899.99,
+        modelUrl: '/product/9',
+        image: '/images/IMG_3665.jpg',
+        iosSrc: '/assets/fender_stratocaster.usdz'
+    },
+    {
+        id: 10,
+        name: 'Tulip Flower',
+        price: 29.99,
+        modelUrl: '/product/10',
+        image: '/images/IMG_3666.jpg',
+        iosSrc: '/assets/flower_tulip.usdz'
+    },
+    {
+        id: 11,
+        name: 'Vintage Gramophone',
+        price: 399.99,
+        modelUrl: '/product/11',
+        image: '/images/IMG_3667.jpg',
+        iosSrc: '/assets/gramophone.usdz'
+    },
+    {
+        id: 12,
+        name: 'Toy Airplane',
+        price: 39.99,
+        modelUrl: '/product/12',
+        image: '/images/IMG_3669.jpg',
+        iosSrc: '/assets/toy_biplane_idle.usdz'
+    },
+    {
+        id: 13,
+        name: 'Toy Car',
+        price: 29.99,
+        modelUrl: '/product/13',
+        image: '/images/IMG_3670.jpg',
+        iosSrc: '/assets/toy_car.usdz'
+    },
+    {
+        id: 14,
+        name: 'Toy Drummer',
+        price: 49.99,
+        modelUrl: '/product/14',
+        image: '/images/IMG_3671.jpg',
+        iosSrc: '/assets/toy_drummer_idle.usdz'
+    },
+    {
+        id: 15,
+        name: 'Retro TV',
+        price: 299.99,
+        modelUrl: '/product/15',
+        image: '/images/IMG_3672.jpg',
+        iosSrc: '/assets/tv_retro.usdz'
+    },
+    {
+        id: 16,
+        name: 'Uberto Chair',
+        price: 199.99,
+        modelUrl: '/product/16',
+        image: '/images/IMG_3673.jpg',
+        iosSrc: '/assets/Uberto_Chair.usdz'
+    }
 ];
